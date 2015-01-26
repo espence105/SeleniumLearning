d218d2f4c7e479381bfa81402640a13e7c27e0eb
espence105 <ejspence@anderson.edu>
2015-01-26 14:59:52 -0500

    working on stuff

--- seleniumProject/src/applicationTest.java
+++ seleniumProject/src/applicationTest.java
@@ -2,21 +2,24 @@
  * Created by Eric on 1/22/2015.
  */
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class applicationTest {
     public static void main(String[] Args){
         WebDriver driver = new FirefoxDriver();
-        String webUrl = "https://10.73.57.253";
+        String webUrl = "http://dev01.cc.ausrc.org/DriverApplication.aspx";
         driver.get(webUrl);
 
-        driver.findElement(By.cssSelector("txtFirstName"));
-
+        findASPElement("txtFirstName", driver).sendKeys("Eric");
 
         driver.close();
-
-
+    }
+    private static WebElement findASPElement(String elementName, WebDriver driver){
+        WebElement test =  driver.findElement(By.xpath("//input[contains(@id,'"+elementName+"' )]"));
+        return test;
 
     }
 }
